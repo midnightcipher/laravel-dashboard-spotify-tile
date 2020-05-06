bc8e875f5080e7ebbd59f264fa9a4609f792dbe3
ashbakernz <ashbakernz@gmail.com>
2020-05-07 00:16:39 +1200

    Style updates

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -1,10 +1,9 @@
 <x-dashboard-tile :position="$position">
-    <div wire:poll.{{ $refreshIntervalInSeconds }}s>
+    <div wire:poll.{{ $refreshIntervalInSeconds }}s
+         class="grid gap-2 justify-items-center h-full text-center"
+         style="grid-template-rows: auto 1fr auto;"
+    >
         @if($isPlaying)
-            <div
-                class="grid gap-2 justify-items-center h-full text-center"
-                style="grid-template-rows: auto 1fr auto;"
-            >
                 <h1 class="font-medium text-dimmed text-sm uppercase tracking-wide">Currently Playing</h1>
                 <div class=" flex items-center justify-center">
                     <img class="w-32 h-32 rounded" src="{{ $albumImage }}" alt="Album Pic">
@@ -25,14 +24,12 @@
                         </span>
                     </div>
                 </div>
-            </div>
         @else
-            <div class="grid grid-rows-auto-1 gap-2 h-full">
-                <div class="flex">
-                    <div class="w-full pt-8 bt-0">
-                        <div class="flex justify-center text-2xl text-grey-darkest font-medium">
-                            <h2>Spotify is not currently playing :(</h2>
-                        </div>
+
+            <div class="flex">
+                <div class="w-full pt-8 bt-0">
+                    <div class="flex justify-center text-2xl text-grey-darkest font-medium">
+                        <h2>Spotify is not currently playing :(</h2>
                     </div>
                 </div>
             </div>
